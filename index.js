ced26774cd8bda6387d65e256ff8cdce05c6f8c8
A-Crawley <crawleyandrew3@gmail.com>
2022-10-30 21:00:36 +1100

    new site

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-const randomPosition = () => {
-  return (
-    Math.random() * ((width < 600 ? width - 50 : width) * 0.75) +
-    (width < 600 ? width - 50 : width) * (0.25 / 2)
-  );
-};
+KUTE.fromTo(
+  "#top1",
+  { path: "#top1" },
+  { path: "#top2" },
+  { repeat: 999, duration: 3000, yoyo: true, easing: "easingCubicInOut" }
+).start();
 
-const randomTime = () => {
-  return Math.random() * (width < 600 ? 3000 : 500);
-};
+KUTE.fromTo(
+  "#mid1",
+  { path: "#mid1" },
+  { path: "#mid2" },
+  { repeat: 999, duration: 3000, yoyo: true, easing: "easingCubicInOut" }
+).start();
 
-const createDroplet = () => {
-  let aubergine = document.createElement("div");
-  aubergine.setAttribute("class", "aubergine spin");
-  aubergine.innerHTML = "🍆";
-  let droplet = document.createElement("div");
-  droplet.setAttribute("class", "droplet drop");
-  droplet.setAttribute("style", `left: ${randomPosition()}px;`);
-  droplet.appendChild(aubergine);
-  return droplet;
-};
-
-let droplets = [];
-
-const height = window.innerHeight;
-const width = window.innerWidth;
-const container = document.querySelector("#container");
-
-setInterval(async () => {
-  const droplets = document.querySelectorAll(".droplet");
-  if (width < 600 ? droplets.length < 3 : droplets.length < 75) {
-    setTimeout(() => {
-      container.appendChild(createDroplet());
-    }, randomTime());
-  }
-
-  droplets.forEach((droplet) => {
-    if (droplet.getClientRects()[0]?.y > height) {
-      droplet.remove();
-    }
-  });
-
-  //console.count(droplets.length);
-}, 50);
+KUTE.fromTo(
+  "#bot1",
+  { path: "#bot1" },
+  { path: "#bot2" },
+  { repeat: 999, duration: 3000, yoyo: true, easing: "easingCubicInOut" }
+).start();
