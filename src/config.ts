847eab240d1ea01a6f7de0937373edd284735464
A-Crawley <crawleyandrew3@gmail.com>
2023-02-26 19:21:26 +1100

    remove log

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@ let process: any;
 
 const p = process?.env ? process.env : import.meta.env;
 
-console.log(p)
-
 export const AirtableConfig = {
     apikey: p.VITE_AIRTABLE_API,
     baseId: p.VITE_BASE_ID
